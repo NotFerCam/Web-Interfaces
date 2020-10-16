54864111c7c83281401e6d48fc844488e098a749
NotFerCam <davidfercam69@gmail.com>
2020-10-16 16:43:26 +0200

    Agregar Archivos

---
+++ temas/Tema 1/Practicas/formulario/visor1.php
@@ -0,0 +1,13 @@
+<?php
+    error_reporting(E_ERROR | E_WARNING | E_PARSE);
+    echo "Nombre: ";
+    echo $_POST['nombre'],"<br>";
+    echo "Apellidos: ";
+    echo $_POST['apellidos'],"<br>";
+    echo "DNI: ";
+    echo $_POST['dni'],"<br>";
+    echo "Genero: ";
+    echo $_POST['genero'],"<br>";
+    echo "Grado a cursar: ";
+    echo $_POST['grado'],"<br>";
+?>
